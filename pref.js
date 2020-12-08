92a20cd215dfb3baefe3d6f84189ff84a07c55bd
oabdaljalil <60644333+oabdaljalil@users.noreply.github.com>
2020-12-08 02:18:59 -0700

    Add files via upload

--- pref.js
+++ pref.js
@@ -1,15 +1,50 @@
-function addItem(){
-  var ul = document.getElementById("dynamic-list");
-  var candidate = document.getElementById("candidate");
+function addItem1(){
+  var ul = document.getElementById("dynamic-list1");
+  var candidate = document.getElementById("candidate1");
   var li = document.createElement("li");
   li.setAttribute('id',candidate.value);
   li.appendChild(document.createTextNode(candidate.value));
   ul.appendChild(li);
 }
 
-function removeItem(){
-  var ul = document.getElementById("dynamic-list");
-  var candidate = document.getElementById("candidate");
+function removeItem1(){
+  var ul = document.getElementById("dynamic-list1");
+  var candidate = document.getElementById("candidate1");
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
+}
+
+
+function addItem2(){
+  var ul = document.getElementById("dynamic-list2");
+  var candidate = document.getElementById("candidate2");
+  var li = document.createElement("li");
+  li.setAttribute('id',candidate.value);
+  li.appendChild(document.createTextNode(candidate.value));
+  ul.appendChild(li);
+}
+
+function removeItem2(){
+  var ul = document.getElementById("dynamic-list2");
+  var candidate = document.getElementById("candidate2");
+  var item = document.getElementById(candidate.value);
+  ul.removeChild(item);
+}
+
+
+
+function addItem3(){
+  var ul = document.getElementById("dynamic-list3");
+  var candidate = document.getElementById("candidate3");
+  var li = document.createElement("li");
+  li.setAttribute('id',candidate.value);
+  li.appendChild(document.createTextNode(candidate.value));
+  ul.appendChild(li);
+}
+
+function removeItem3(){
+  var ul = document.getElementById("dynamic-list3");
+  var candidate = document.getElementById("candidate3");
   var item = document.getElementById(candidate.value);
   ul.removeChild(item);
 }
